290e4a148c5459d06706c3d444a6c2ae7d1011ad
gaurav dubey <gaurav.dubey020@gmail.com>
2026-05-14 07:42:18 +0530

    Fixed Render port issue

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -41,6 +42,5 @@ def predict():
             'index.html',
             prediction_text=f'Error: {str(e)}'
         )
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
